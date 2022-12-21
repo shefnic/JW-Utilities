5f4d344ff0e7900a80fb12355a3d33b320c5dffb
Nicholas Shefte <shefnic@gmail.com>
2022-12-20 21:36:36 -0600

    Added project files

---
+++ Extensions/DateTimeExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace JWUtilities.Extensions;
+
+public static class DateTimeExtensions
+{
+    public static bool IsAfter(this DateTime baseDT, DateTime compareDT)
+    {
+        return baseDT < compareDT;
+    }
+
+    public static bool IsOnOrAfter(this DateTime baseDT, DateTime compareDT)
+    {
+        return baseDT <= compareDT;
+    }
+
+    public static bool IsBefore(this DateTime baseDT, DateTime compareDT)
+    {
+        return baseDT > compareDT;
+    }
+
+    public static bool IsOnOrBefore(this DateTime baseDT, DateTime compareDT)
+    {
+        return baseDT >= compareDT;
+    }
+}
---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace JWUtilities.Extensions;
+
+public static class StringExtensions
+{
+    public static bool EqualsIgnoreCase(this string baseString, string compareString)
+    {
+        return baseString.Equals(compareString, StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    public static bool ContainsIgnoreCase(this string baseString, string compareString)
+    {
+        return baseString.Contains(compareString, StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    public static bool StartsWithIgnoreCase(this string baseString, string compareString)
+    {
+        return baseString.StartsWith(compareString, StringComparison.InvariantCultureIgnoreCase);
+    }
+}
